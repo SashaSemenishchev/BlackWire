ab00e61dea33a95e4a0f5dca45f618994963150c
Sasha Semenishchev <sashasemenishchev@gmail.com>
2021-06-19 21:38:38 +0300

    Authentication message sending to server

--- BlackWireClient/src/main/java/me/mrfunny/blackwire/client/SocketHandler.java
+++ BlackWireClient/src/main/java/me/mrfunny/blackwire/client/SocketHandler.java
@@ -1,28 +1,82 @@
 package me.mrfunny.blackwire.client;
 
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import me.mrfunny.blackwire.socket.message.Message;
+import me.mrfunny.blackwire.socket.message.MessageType;
+import me.mrfunny.blackwire.util.RSAUtil;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+import java.util.HashMap;
 
 public class SocketHandler implements Runnable {
     private final int port;
     private final String host;
+    private final HashMap<String, Object> data;
     private Socket socket;
     private ObjectInputStream in;
     private ObjectOutputStream out;
 
-    public SocketHandler(String host, int port) throws IOException {
+    public SocketHandler(String host, int port, HashMap<String, Object> data) throws IOException {
         this.host = host;
         this.port = port;
+        this.data = data;
     }
 
     @Override
     public void run() {
-        while(socket.isConnected()){
-            try {
+        try {
+            Message response = sendPacketMessage(new Message(MessageType.ACTION, "publickey"));
+            PublicKey serverPublic = RSAUtil.fromStringToPublicKey(response.getMessage());
+            HashMap<String, Object> payload = new HashMap<>();
+            boolean usingPassword = false;
+            if(data.containsKey("token")){
+                payload.put("token", RSAUtil.encrypt(serverPublic, data.get("token").toString()));
+            } else if(data.containsKey("password")){
+                payload.put("username", data.get("username"));
+                payload.put("password", RSAUtil.encrypt(serverPublic, data.get("password").toString()));
+                usingPassword = true;
+            } else {
+                Platform.runLater(() -> {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText("Password needed");
+                    alert.showAndWait();
+                });
+            }
+
+            Message authMessage;
+            if(usingPassword){
+                if(data.containsKey("repeatPassword")){
+                    authMessage = new Message(MessageType.ACTION, "register", payload);
+                } else {
+                    authMessage = new Message(MessageType.ACTION, "login", payload);
+                }
+            } else {
+                authMessage = new Message(MessageType.ACTION, "auth", payload);
+            }
+
+            Message tokenResponse = sendPacketMessage(authMessage);
+            if(tokenResponse.getMessage().toLowerCase().contains("error")){
+                Platform.runLater(() -> {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText(tokenResponse.getMessage());
+                    alert.showAndWait();
+                });
+                return;
+            }
+            // TODO: Save token, show chat window etc.
+            while(socket.isConnected()){
                 Message message = (Message) in.readObject();
                 switch (message.getMessageType()){
 
@@ -35,9 +89,20 @@ public class SocketHandler implements Runnable {
                     case ACTION:
                         break;
                 }
-            } catch (IOException | ClassNotFoundException exception) {
-                exception.printStackTrace();
             }
-        }
+        } catch (IOException | ClassNotFoundException | NoSuchAlgorithmException | InvalidKeySpecException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException exception) {
+        exception.printStackTrace();
+    }
+    }
+
+    public void sendMessage(Message message) throws IOException {
+        out.writeObject(message);
+        out.flush();
+    }
+
+    public Message sendPacketMessage(Message message) throws IOException, ClassNotFoundException {
+        out.writeObject(message);
+        out.flush();
+        return (Message) in.readObject();
     }
 }
--- BlackWireClient/src/main/java/me/mrfunny/blackwire/socket/message/Message.java
+++ BlackWireClient/src/main/java/me/mrfunny/blackwire/socket/message/Message.java
@@ -2,13 +2,58 @@ package me.mrfunny.blackwire.socket.message;
 
 import org.apache.commons.codec.binary.Base64;
 
+import java.util.HashMap;
+
 public class Message {
     private final MessageType messageType;
-    private final byte[] content;
+    private byte[] content = new byte[1024];
+    private HashMap<String, Object> additionalData = new HashMap<>();
+    private String action;
+    private String message;
 
     public Message(MessageType messageType, byte[] content) {
         this.messageType = messageType;
         this.content = content;
+    }
+
+    public Message(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public Message(MessageType messageType, HashMap<String, Object> data) {
+        this.messageType = messageType;
+        this.additionalData = data;
+    }
+
+    public Message(MessageType messageType, String action) {
+        this.messageType = messageType;
+        this.action = action;
+    }
+
+    public Message(MessageType messageType, String action, HashMap<String, Object> data) {
+        this.messageType = messageType;
+        this.action = action;
+        this.additionalData = data;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HashMap<String, Object> getData() {
+        return additionalData;
+    }
+
+    public void addData(String key, Object data){
+        additionalData.put(key, data);
+    }
+
+    public void removeData(String key){
+        additionalData.remove(key);
     }
 
     public MessageType getMessageType() {
@@ -25,5 +70,9 @@ public class Message {
 
     public String getBase64StringContent(){
         return Base64.encodeBase64String(content);
+    }
+
+    public String getAction() {
+        return action;
     }
 }
